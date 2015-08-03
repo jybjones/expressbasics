c15f03db34b54a80452e84637dbb7a9c2d659579
Jennifer Jones <jybjones@gmail.com>
2015-08-03 11:43:18 -0500

    changed ReadMe file and also added EJS templates and routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # expressbasics
+
+First day using Express and learning how to create routes and error handling.
+ 400 --> server error
+ 500 service error --> user error
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express(); //executing Express and named app
 // var app = require('express')();--> same thing in ONE line
+app.set('view engine', 'ejs');
+
+app.use(function(req, res, next) {
+  ///logging at the top
+  console.log('Request at ' + new Date().toISOString());
+  next(); //call next for middleware WHEN there's other things after! OTherwise, chain will end
+});
 
 app.use(express.static('public'));
 
@@ -12,16 +19,31 @@ app.get('/', function (req, res) { //get method at route, and callback method wh
 // app.get('/', function (req, res) {
 //   res.send('This is the Root!');
 // });
-app.get('/World', function (req, res) {
-  res.send('World');
+app.get('/awesomethings', function (req, res) {
+  setTimeout(function (){
+    var awesomeThings = [
+    'Pizza',
+    'Bacon',
+    '2nd Amendment',
+    'Pluto',
+    'Space Jam'
+  ];
+  res.render('templates/world',
+    { title: 'Awesomesite.com',
+       welcome: 'Thanks for coming by!',
+       awesomeThings: awesomeThings
+     }
+    );
+  }, 5000);
+});
+
+app.get('/test', function (req, res, next) {
+  res.write('Test1');
+  next();
 });
 
 app.get('/test', function (req, res) {
-  res.send('Test1');
-});
-
-app.get('/test', function (req, res) {
-  res.send('Test2');
+  res.end('Test2');
 });
 
 app.get('/json', function (req, res) {
@@ -33,14 +55,14 @@ app.get('/thisshoulderror', function (req, res) {
   res.send(badVariable);
 });
 
+app.use(function(req, res) {
+  res.status(403).send('Unauthorized!!');
+});
+
 ////NEED to pass 4 ARGUMENTS to create an error handling!!!
 app.use(function (err, req, res, next) {
   console.log('ERRRRRR', err.stack);
   res.status(500).send('My Bad')
-});
-
-app.use(function(req, res) {
-  res.status(403).send('Unauthorized!!');
 });
 
 
