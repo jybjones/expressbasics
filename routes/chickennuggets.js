1540e1d52b8ac9f2859453f7d5e6b65a3b066a4e
Jennifer Jones <jybjones@gmail.com>
2015-08-07 12:05:46 -0500

    added ObjectID variable to extract the _id info when we complete an order

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
 
 var router = express.Router();
 
@@ -10,9 +11,10 @@ router.get('/', function (req, res) {
   collection.find().toArray(function(err, orders) {
     var formattedOrders = orders.map(function (order) {
       return {
-        name: order.name,
-        flavor: order.style,
-        qty: order.qty,
+        _id:       order._id,
+        name:      order.name,
+        flavor:    order.style,
+        qty:       order.qty,
         createdAt: moment(order._id.getTimestamp()).fromNow()
       };
     });
@@ -35,6 +37,14 @@ router.post('/order', function (req, res) {
   // res.send('Thanks for ordering!');
 //   res.redirect('/'); ////redirected to Hello World after placing order
 // });
-
+router.post('/order/:id/complete', function (req, res) {
+  var collection = global.db.collection('chickenNuggets');
+    collection.update(
+      {_id:     ObjectID(req.params.id)},
+      {$set:    {complete: true}},
+      function(){
+        res.redirect('/chickennuggets')
+    });
+  });
 
 module.exports = router;
