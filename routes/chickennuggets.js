b483d1a25c2a3243c5a24add967fa5befe39efd7
Jennifer Jones <jybjones@gmail.com>
2015-08-07 11:15:36 -0500

    added moment library to get time since order placed

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,15 +1,33 @@
 var express = require('express');
+var moment = require('moment');
+
 var router = express.Router();
+
 
 //get function want a new template!//
 router.get('/', function (req, res) {
-  res.render('templates/chickennuggets');
+  var collection = global.db.collection('chickenNuggets');
+  collection.find().toArray(function(err, orders) {
+    var formattedOrders = orders.map(function (order) {
+      return {
+        name: order.name,
+        flavor: order.style,
+        qty: order.qty,
+        createdAt: moment(order._id.getTimestamp()).fromNow()
+      };
+    });
+    res.render('templates/chicken-index', {orders: formattedOrders}); //this is the list of orders
+  });
+});
+
+router.get('/order', function (req, res) {
+  res.render('templates/chicken-new'); //order form
 });
 
 router.post('/order', function (req, res) {
   var collection = global.db.collection('chickenNuggets');
     collection.save(req.body, function(){
-      res.redirect('/');
+      res.redirect('/chickennuggets');
     });
   });
 
